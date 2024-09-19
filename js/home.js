d502d98246708e4c3c3394d0658a3f440e20a7f5
limon <limonsk026@gmail.com>
2024-09-19 08:36:49 +0600

    Add money button event handeler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log("Home added");
+// Add money event handaler
+document
+  .getElementById("button-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    
+    // Get money added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+    // Get the pin number
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+  });
